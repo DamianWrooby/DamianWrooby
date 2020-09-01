be480b72f33f9450a5c690a2d0cf5437934a224a
Damian Wróblewski <36818809+DamianWrooby@users.noreply.github.com>
2020-09-01 20:35:22 +0200

    Update twitter.R

--- twitter.R
+++ twitter.R
@@ -12,7 +12,7 @@ lasttweet_token <- function() {
   )
 }
 
-handle <- "DamianWrooby"
+handle <- "damianwrooby"
 recent_tweets <- get_timeline(handle, n = 1, token = lasttweet_token())
 
 tmpimg <- "tweet.png"
